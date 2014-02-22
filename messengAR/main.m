0d967bf399d603022fbd5874599f405258ee7618
Dan Mundy <drm.mundy@gmail.com>
2014-02-22 14:38:59 -0500

    Adds a new Xcode project

---
+++ messengAR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  messengAR
+//
+//  Created by Dan Mundy on 2/22/14.
+//  Copyright (c) 2014 strangerware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSGRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSGRAppDelegate class]));
+    }
+}
---
+++ messengARTests/messengARTests.m
@@ -0,0 +1,34 @@
+//
+//  messengARTests.m
+//  messengARTests
+//
+//  Created by Dan Mundy on 2/22/14.
+//  Copyright (c) 2014 strangerware. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface messengARTests : XCTestCase
+
+@end
+
+@implementation messengARTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
